9726d24923735b39cee19dddd3de4741eff9d858
Kiyonobu Itahashi <keyi8773@yahoo.co.jp>
2021-10-26 13:33:15 +0900

    Update setting.sh

--- setting.sh
+++ setting.sh
@@ -7,7 +7,9 @@ xcode-select --install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 ## install java first
-brew cask java
+brew install openjdk
+echo 'export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"' >> ~/.zshrc
+export CPPFLAGS="-I/opt/homebrew/opt/openjdk/include"
 
 ## install ansible
 brew install ansible
