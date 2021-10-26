ece276172a458cb61070d46df5fd4be3c44c866b
Kiyonobu Itahashi <keyi8773@yahoo.co.jp>
2021-10-26 13:25:27 +0900

    Update setting.sh

--- setting.sh
+++ setting.sh
@@ -4,7 +4,7 @@
 xcode-select --install
 
 ## install homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 ## install java first
 brew cask java
